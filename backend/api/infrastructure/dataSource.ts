bcb994ad03fbe4acefa355526303620e901c7f5a
KevinGalvisSA <156678719+KevinGalvisSA@users.noreply.github.com>
2025-06-27 13:55:03 -0500

    feat: :sparkles: Configuracion de la base de datos y conexion

--- backend/api/infrastructure/dataSource.ts
+++ backend/api/infrastructure/dataSource.ts
@@ -1,17 +1,22 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../domain/models/User.ts";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "Kevin",
-    password: "testeando",
-    database: "mypostbase",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "####"),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true, // Solo para desarrollo
     logging: true,
     entities: [User],
     migrations: [],
     subscribers: [],
 });
+
+//postgres://camper:camper2023@localhost:5432/mypsqlbase
